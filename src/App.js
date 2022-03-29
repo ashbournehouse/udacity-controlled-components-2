2b49eda3e51801026023c0d65097638b125ae1cf
Andrew Biller <a.biller@ashho.net>
2022-03-29 11:57:09 +0100

    AddItem componentised

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Css/App.css';
 
 import Header from './Components/Header.jsx';
-//import AddItem from './Components/AddItem.jsx';
+import AddItem from './Components/AddItem.jsx';
 import ItemList from './Components/ItemList.jsx';
 
 
@@ -12,14 +12,24 @@ class App extends React.Component {
     items: [],
   };
 
+  /*
   handleChange = event => {
     this.setState({ value: event.target.value });
   };
+  */
 
+  /*
   addItem = event => {
     event.preventDefault();
     this.setState(oldState => ({
       items: [...oldState.items, this.state.value],
+    }));
+  };
+  */
+
+  updateList = (textValue) => {
+    this.setState(oldState => ({
+      items: [...oldState.items, textValue],
     }));
   };
 
@@ -27,6 +37,7 @@ class App extends React.Component {
     this.setState(prevState => ({ items: this.state.items.slice(0, -1) }));
   };
 
+  /*
   inputIsEmpty = () => {
     return this.state.value === '';
   };
@@ -34,25 +45,19 @@ class App extends React.Component {
   noItemsFound = () => {
     return this.state.items.length === 0;
   };
+  */
 
   render() {
     return (
       <div className="App">
         <Header />
-        <h2>Shopping List</h2>
-        <form onSubmit={this.addItem}>
-          <input
-            type="text"
-            placeholder="Enter New Item"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-          <button disabled={this.inputIsEmpty()}>Add</button>
-        </form>
-
-        <button onClick={this.deleteLastItem} disabled={this.noItemsFound()}>
-          Delete Last Item
-        </button>
+        <h4>List length is: {this.state.items.length}</h4>
+        <AddItem
+          listLength = {this.state.items.length}
+          addItem = {this.addItem}
+          updateList = {this.updateList}
+          deleteLastItem = {this.deleteLastItem}
+        />
         <ItemList items={this.state.items} />
       </div>
     );
--- src/Components/AddItem.jsx
+++ src/Components/AddItem.jsx
@@ -3,31 +3,53 @@ import React from 'react';
 class AddItem extends React.Component {
   constructor(props) {
     super(props);
+    this.updateList = this.props.updateList.bind(this);
+    this.deleteLastItem = this.props.deleteLastItem.bind(this);
+    const listLength = this.props.listLength;
+    //const listLength = 0;
+  };
+
+  state = {
+    textBoxValue: '',
   };
 
   handleChange = (event) => {
-    this.setState({ value: event.target.value });
+    this.setState({ textBoxValue: event.target.value });
   };
   
   addItem = (event) => {
     event.preventDefault();
-    this.setState(oldState => ({
-      items: [...oldState.items, this.state.value],
-    }));
+    this.updateList(this.capitaliseString(this.state.textBoxValue));
+    this.clearInput();
   };
-  
-  deleteLastItem = (event) => {
-    this.setState(prevState => ({ items: this.state.items.slice(0, -1) }));
+
+  capitaliseString(value) {
+    return value.charAt(0).toUpperCase() + value.toLowerCase().slice(1);
+  }
+
+  clearInput = () => {
+    this.setState(() => ({ textBoxValue: '' }))
+  };
+
+    // inputIsEmpty is used to disable the addItem button
+  inputIsEmpty = () => {
+    return this.state.textBoxValue === '';
+  };
+
+    // noItemsFound is used to disable the removeLastItem button
+  noItemsFound = (listLength) => {
+    return (listLength === 0);
   };
   
   render() {
     return(
       <div className="controls" style={{backgroundColor:'#000066', padding:'0.5em'}}>
         <h2>Shopping List</h2>
+        <h4>List length is: {this.props.listLength}</h4>
         <input
           type="text"
           placeholder="Enter New Item"
-          value={this.state.value}
+          value={this.state.textBoxValue}
           onChange={this.handleChange}
         />
         
@@ -39,7 +61,7 @@ class AddItem extends React.Component {
 
         <button 
           onClick={this.deleteLastItem}
-          disabled={this.noItemsFound()}>
+          disabled={this.noItemsFound(this.props.listLength)}>
           Delete Last Item
         </button>
       </div>
